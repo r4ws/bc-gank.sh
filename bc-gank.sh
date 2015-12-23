410d1bd460f9e516e175c5e9d642ee9b9ea2877b
Raws <raws@dataturn.net>
2015-12-23 07:14:50 -0600

    Update bc-gank.sh

--- bc-gank.sh
+++ bc-gank.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR=`echo "$ARTIST"-"$ALBUM" | sed 's/ /_/g'`
-/opt/youtube-dl/youtube-dl --restrict-filenames -j $1 > /tmp/scrape.json
+youtube-dl --restrict-filenames -j $1 > /tmp/scrape.json
 # artist name.  not included in json so curl and grab it from the html, set as variable
 ARTIST=`curl -s $1 | grep -m1  -i "artist:" | tr -d ',' | tr -s ': ' ':' | awk -F':' '{print $3}'`
 # album name
@@ -14,7 +14,6 @@ convert big.jpg -resize 50% "$DIR/folder.jpg"
 jp2a --width=75 --colors "$DIR/folder.jpg" > /tmp/ascii.art
 # create list of stream urls to curl
 jq '.url' /tmp/scrape.json > $DIR/url.info
-#curl -s $1 | grep 'mp3' | sed 's/    trackinfo : //g' | sed 's/.$//' | jq '.[] | .file' | grep mp3 | awk '{print $2}' > $DIR/url.info
 # create filename list for tagging id3 
 jq '._filename' /tmp/scrape.json > $DIR/filename.info
 # create list of titles to id3tag per filename
@@ -25,7 +24,6 @@ TRACKCOUNT=`wc -l < "$DIR"/url.info`
 TR=`expr $TRACKCOUNT + 1`
 # set up out general purpose counter
 LINE=0
-#/opt/youtube-dl/bin/youtube-dl --restrict-filenames $1
 ALBUMT=`echo $ALBUM | tr -d '"'`
 ARTISTT=`echo $ARTIST | tr -d '"'`
 while [ "$LINE" -lt "$TRACKCOUNT" ]
@@ -35,7 +33,6 @@ do
         URL=`sed ''$LINE'q;d' url.info | tr -d '"'`
 	curl -s -L "$URL" > "$DIR"/"$FTITLE" &
 done
-#COUNT=`pgrep -f curl -c`
 # define jthe amount of processes we know are remaining. ouput the curl processes still remaining in psudo realtime  untill they complete
 while [ "$COUNT" != "0" ]
 do
